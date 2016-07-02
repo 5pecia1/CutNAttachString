8ebc35780ae2e840707e465d8fd1487a35c44a5f
5pecia1 <anmenmolsu22@naver.com>
2016-07-03 00:58:05 +0900

    rename all

--- Sample/src/sample/Main.java
+++ Sample/src/sample/Main.java
@@ -10,20 +10,20 @@ public class Main {
 		
 		System.out.println("default string : " + defaultString);
 		
-		String pureString = DivideStringInterval.makePureString(defaultString, dot);
+		String pureString = CutNAttachStringInterval.makePureString(defaultString, dot);
 		System.out.println("make pure string : " + pureString);
 		
-		String dividedString = DivideStringInterval.divideStringInterval(pureString, dot, 4);
+		String dividedString = CutNAttachStringInterval.divideString(pureString, dot, 4);
 		System.out.println("divide pure string(interval 4) : " + dividedString);
 		
-		String dividedPureString = DivideStringInterval.makePureNDivideStringInterval(dividedString, dot, 5);
-		System.out.println("divide dividedString(interval 5) : " + dividedPureString);
+		String dividedPureString = CutNAttachStringInterval.makePureNDivideString(dividedString, dot, 5);
+		System.out.println("make pure and divide string(interval 5) : " + dividedPureString);
 		
 		String appendString = "0";
-		String appendedString = DivideStringInterval.append(dividedPureString, dot, 5, appendString);
+		String appendedString = CutNAttachStringInterval.append(dividedPureString, dot, 5, appendString);
 		System.out.println("append string(interval 5) : " + appendedString);
 		
-		String deletedString = DivideStringInterval.deleteEnd(appendedString, dot, 5);
+		String deletedString = CutNAttachStringInterval.deleteEnd(appendedString, dot, 5);
 		System.out.println("delete string(interval 5) : " + deletedString);
 	}
 }
